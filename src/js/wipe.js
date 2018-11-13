950d7ec6f47b06d6e0630fc0f43055dbd3e13adf
Losimi <2447800724@qq.com>
2018-11-13 10:08:15 +0800

    增加移动端知识

--- src/js/wipe.js
+++ src/js/wipe.js
@@ -10,14 +10,43 @@ var RY=0;
 // 鼠标的状态
 var ismouseDown=false;
 // 默认是未按下去false，按下去true
+//兼容手机各类
+var device = (/android|webos|iPhone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+console.log(navigator.userAgent);
+console.log(device);
+var clickEvtName = device ? "touchstart" : "mousedown";
+var moveEvtName = device ? "touchmove" : "mousemove";
+var endEvtName = device ? "touchend" : "mouseup";
+// console.log(argunments);
 //生成画布上的遮罩，默认颜色#666 ;
 function drawmask(context){
 	context.fillStyle="#666";
 	context.fillRect(0,0,_w,_h);
 	context.globalCompositeOperation="destination-out";
 }
+// function drawtong(context,RX,RY,starX,starY,endX,endY){
+// 	if (argunments.length==3) {
+// 	context.save();  
+// 	context.beginPath();
+// 	context.arc(RX,RY,radius,0,2*Math.PI);
+// 	context.fillStyle="red";
+// 	context.fill();
+// 	context.restore();
+// 	}
+// 	if (argunments.length==5) {
+// 	context.save();
+// 	context.lineCap="round";
+// 	context.lineWidth=2*radius;
+// 	context.beginPath();
+// 	context.moveTo(starX,starY);
+// 	context.lineTo(endX,endY);
+// 	context.stroke();
+// 	context.restore();	
+// 	}
+// }
 function drawPoint(context,RX,RY){
-	context.save();
+	// console.log("实参个数"+argunments.length)
+	context.save();  
 	context.beginPath();
 	context.arc(RX,RY,radius,0,2*Math.PI);
 	context.fillStyle="red";
@@ -25,6 +54,7 @@ function drawPoint(context,RX,RY){
 	context.restore();
 }
 function drawxian(context,starX,starY,endX,endY){
+	// console.log("实参个数"+argunments.length)
 	context.save();
 	context.lineCap="round";
 	context.lineWidth=2*radius;
@@ -34,25 +64,19 @@ function drawxian(context,starX,starY,endX,endY){
 	context.stroke();
 	context.restore();
 }
-// 在canvas中添加自定义监听事件"mousedown",调用drawPoint函数 
-canvas.addEventListener("mousedown",function(evt){
+canvas.addEventListener(clickEvtName,function(evt){
 	ismouseDown=true;
 	var event=evt||window.event;
 	//获取鼠标在视口的坐标，传递参数到drawPoint;
-	RX=event.clientX;
-	RY=event.clientY;
+	RX=device ? event.touches[0].clientX:event.clientX;
+	RY=device ? event.touches[0].clientY:event.clientY;
 	drawPoint(context,RX,RY);
-	
 },false);
-//增加监听“mousemove”，调用drawpoint
-canvas.addEventListener("mousemove",function(evt){
-	var event=evt||window.event;
-	// var aX=event.clientX;
-	// var aY=event.clientY;
-	// drawPoint(context,aX,aY);
-	if (ismouseDown==true) {	
-	var aX2=event.clientX;
-	var aY2=event.clientY;
+canvas.addEventListener(moveEvtName,function(evt){
+	if (ismouseDown) {
+	event.preventDefault();	
+	var aX2=device ? event.touches[0].clientX:event.clientX;
+	var aY2=device ? event.touches[0].clientY:event.clientY;
 	drawxian(context,RX,RY,aX2,aY2);
 	//每次的结束点变成下一次开始的点。
 	RX=aX2;
@@ -61,11 +85,37 @@ canvas.addEventListener("mousemove",function(evt){
 		return false;
 	}
 },false);
-canvas.addEventListener("mouseup",function(){
+// 在canvas中添加自定义监听事件"mousedown",调用drawPoint函数 
+// canvas.addEventListener("mousedown",function(evt){
+// 	ismouseDown=true;
+// 	var event=evt||window.event;
+// 	//获取鼠标在视口的坐标，传递参数到drawPoint;
+// 	RX=event.clientX;
+// 	RY=event.clientY;
+// 	drawPoint(context,RX,RY);
+// },false);
+//增加监听“mousemove”，调用drawpoint
+// canvas.addEventListener("mousemove",function(evt){
+// 	var event=evt||window.event;
+// 	// var aX=event.clientX;
+// 	// var aY=event.clientY;
+// 	// drawPoint(context,aX,aY);
+// 	if (ismouseDown==true) {	
+// 	var aX2=event.clientX;
+// 	var aY2=event.clientY;
+// 	drawxian(context,RX,RY,aX2,aY2);
+// 	//每次的结束点变成下一次开始的点。
+// 	RX=aX2;
+// 	RY=aY2;
+// 	}else{
+// 		return false;
+// 	}
+// },false);
+canvas.addEventListener(endEvtName,function(){
 	//还原ismouseDown，为false。
 	ismouseDown=false;
 	if (getTRansparencyPercent(context)>50) {
-		alert("超过50%面积");
+		// alert("超过50%面积");
 		clear(context);
 	}
 },false);
@@ -90,4 +140,5 @@ window.onload=function(){
 	drawmask(context);
 	drawPoint(context);	
 	drawxian(context);
+	// drawtong(context);
 }
