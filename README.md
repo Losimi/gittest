503efcc5a4acfb15e948c035075cb9113ffdbc17
Losimi <2447800724@qq.com>
2018-11-19 17:19:01 +0800

    3.0.1版本

--- README.md
+++ README.md
@@ -3,3 +3,57 @@ PC端实现涂抹擦除效果, 超过50%的涂抹面积可以查看全部. 涂�
 ## ver 1.0.0 ##
 1. 实现了对移动端的支持
 1. 函数优化
+## var 2.0.0  ##
+实现了面向对象方式，
+增加了参数配置
+## var3.0.0 ##
+1、浏览器在滚动距离下bug修复
+2、canvas画布在有偏移和绝对定位下bug修复
+3、增加了回调函数，让用户可以自己完成后继功能
+
+
+使用步骤说明：
+1、在html中添加指定id的canvas标签
+例如
+``` 
+<canvas id="cas" width="375" height="665"></canvas>
+ ```
+
+2、编辑配置文件：
+|属性名|取值类型|备注|
+|id|字符串|canvas标签的id|
+|covertype|字符串|取值"color"或"image"|
+|color|字符串|十六进制颜色吗，或rgb()|git 
+|imgUrl|字符串|前面的覆盖图片|
+|backImgUrl|字符串|canvas背景图片|
+|width|字符串|canvas宽度，必须和canvas标签宽度一致|
+|height|字符床|canvas高度，必须和canvas标签高粗一致|
+|radius|字符串|涂抹笔的半径|
+|transpercent|数值|透明面积占整个画布的百分比，超出此数值显示全部化布|
+|callback、|函数|用户自定义回调函数名称|
+``` 
+var wipeConfjg = {
+	id:"cas",
+	coverType:"image",   //取值类型color,image
+	color:"#fff66",
+	imgUrl:"image/wipe2.jpg",   //前面的覆盖图
+	backImgUrl:"image/wipe1.jpg",  //背景图片
+	width:"375",                   //canvas宽
+	height:"665",                 //canvas高
+	radius:"25",            //涂抹笔的半径
+	percent:55, //透明面积占整个面积画布的百分比，超出此数字显示全部名称
+	callback:wibedCallbackk //用户自定义回调函数
+	}
+ ``` 
+ 
+ 3、初始化wipe插件，并将上一步的配置作为变量传入例如：
+ ``` 
+new wipe（wipeConfig）
+ ```
+ 4、编写回调函数，在用户在涂抹完成后继续操作必须写次回调函数
+ ``` 
+	function wibedCallbackk(percent){
+	if (percent >50) {
+
+	}
+ ```
---
+++ src/js/wipelod.js
@@ -0,0 +1,147 @@
+var canvas = document.getElementById('cas');
+var context = canvas.getContext("2d");
+var _w=canvas.width;
+var _h=canvas.height;
+var num=0;
+// 涂抹的半径
+var radius=20;
+var RX=0;
+var RY=0;
+// 鼠标的状态
+var ismouseDown=false;
+// 默认是未按下去false，按下去true
+//兼容手机各类
+var device = (/android|webos|iPhone|ipad|ipod|blackberry|iemobile|opera mini/i.test(navigator.userAgent.toLowerCase()));
+console.log(navigator.userAgent);
+console.log(device);
+var clickEvtName = device ? "touchstart" : "mousedown";
+var moveEvtName = device ? "touchmove" : "mousemove";
+var endEvtName = device ? "touchend" : "mouseup";
+// console.log(argunments);
+//生成画布上的遮罩，默认颜色#666 ;
+function drawmask(context){
+	context.fillStyle="#666";
+	context.fillRect(0,0,_w,_h);
+	context.globalCompositeOperation="destination-out";
+}
+function drawtong(context,RX,RY,aX2,aY2){
+	if (arguments.length===3) {
+	context.save();  
+	context.beginPath();
+	context.arc(RX,RY,radius,0,2*Math.PI);
+	context.fillStyle="red";
+	context.fill();
+	context.restore();
+	}else if(arguments.length===5){
+	context.save();
+	context.lineCap="round";
+	context.lineWidth=2*radius;
+	context.beginPath();
+	context.moveTo(RX,RY);
+	context.lineTo(aX2,aY2);
+	context.stroke();
+	context.restore();	
+	}else{
+	return false;
+	}
+}
+// function drawPoint(context,RX,RY){
+// 	// console.log("实参个数"+argunments.length)
+// 	context.save();  
+// 	context.beginPath();
+// 	context.arc(RX,RY,radius,0,2*Math.PI);
+// 	context.fillStyle="red";
+// 	context.fill();
+// 	context.restore();
+// }
+// function drawxian(context,starX,starY,endX,endY){
+// 	// console.log("实参个数"+argunments.length)
+// 	context.save();
+// 	context.lineCap="round";
+// 	context.lineWidth=2*radius;
+// 	context.beginPath();
+// 	context.moveTo(starX,starY);
+// 	context.lineTo(endX,endY);
+// 	context.stroke();
+// 	context.restore();
+// }
+canvas.addEventListener(clickEvtName,function(evt){
+	ismouseDown=true;
+	var event=evt||window.event;
+	//获取鼠标在视口的坐标，传递参数到drawPoint;
+	RX=device ? event.touches[0].clientX:event.clientX;
+	RY=device ? event.touches[0].clientY:event.clientY;
+	// drawPoint(context,RX,RY);
+	drawtong(context,RX,RY);
+},false);
+canvas.addEventListener(moveEvtName,function(evt){
+	if (ismouseDown) {
+	event.preventDefault();	
+	var aX2=device ? event.touches[0].clientX:event.clientX;
+	var aY2=device ? event.touches[0].clientY:event.clientY;
+	// drawxian(context,RX,RY,aX2,aY2);
+	drawtong(context,RX,RY,aX2,aY2);
+	//每次的结束点变成下一次开始的点。
+	RX=aX2;
+	RY=aY2;
+	}else{
+		return false;
+	}
+},false);
+// 在canvas中添加自定义监听事件"mousedown",调用drawPoint函数 
+// canvas.addEventListener("mousedown",function(evt){
+// 	ismouseDown=true;
+// 	var event=evt||window.event;
+// 	//获取鼠标在视口的坐标，传递参数到drawPoint;
+// 	RX=event.clientX;
+// 	RY=event.clientY;
+// 	drawPoint(context,RX,RY);
+// },false);
+//增加监听“mousemove”，调用drawpoint
+// canvas.addEventListener("mousemove",function(evt){
+// 	var event=evt||window.event;
+// 	// var aX=event.clientX;
+// 	// var aY=event.clientY;
+// 	// drawPoint(context,aX,aY);
+// 	if (ismouseDown==true) {	
+// 	var aX2=event.clientX;
+// 	var aY2=event.clientY;
+// 	drawxian(context,RX,RY,aX2,aY2);
+// 	//每次的结束点变成下一次开始的点。
+// 	RX=aX2;
+// 	RY=aY2;
+// 	}else{
+// 		return false;
+// 	}
+// },false);
+canvas.addEventListener(endEvtName,function(){
+	//还原ismouseDown，为false。
+	ismouseDown=false;
+	if (getTRansparencyPercent(context)>50) {
+		// alert("超过50%面积");
+		clear(context);
+	}
+},false);
+function getTRansparencyPercent(context){
+	var Imgdata = context.getImageData(0,0,_w,_h);
+	for (var i = 0; i < Imgdata.data.length; i+=4) {
+		var a =Imgdata.data[i+3];
+		if (a===0) {
+			num++;
+		}
+	}
+	var percent=(num/(_w*_h))*100;
+	console.log("透明点的个数："+num);
+	console.log("占的面积"+Math.ceil(percent)+"%");
+	// return percent.toFixded(2);
+	return Math.round(percent);
+}
+function clear(context){
+	context.clearRect(0,0,_w,_h);
+}
+window.onload=function(){
+	drawmask(context);
+	// drawPoint(context);	
+	// drawxian(context);
+	drawtong(context);
+}
